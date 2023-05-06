6a8c443d94c60dadc41c89a394b66d58750bab9c
Rafeeq <rsyed0904@gmail.com>
2023-05-06 12:40:03 +0530

    installed necessary packages

--- Web/src/main.jsx
+++ Web/src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>,
   document.getElementById('root')
 );
