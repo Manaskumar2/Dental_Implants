19ff0b46fed668c4ca416c1a64eca632566063a0
Manas Kumar Jena <109361353+Manaskumar2@users.noreply.github.com>
2023-05-09 16:14:34 +0530

    Delete Services directory
    delelete

--- Services/src/routes/route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { createAppointement } = require("../controller/appointementController");
-
-router.post ("/createAppontments",createAppointement)
-
-
-module.exports =router
